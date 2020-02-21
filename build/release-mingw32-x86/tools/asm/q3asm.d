95333f55b13dc71008614d1ddd075259d8f5acf7
{Z}Mike <32443002+MikeMorals@users.noreply.github.com>
2020-02-21 14:22:26 -0600

    removed gitingore

---
+++ build/release-mingw32-x86/tools/asm/q3asm.d
@@ -0,0 +1,3 @@
+build/release-mingw32-x86/tools/asm/q3asm.o: src/tools/asm/q3asm.c \
+  src/tools/asm/cmdlib.h src/tools/asm/mathlib.h \
+  src/tools/asm/../../qcommon/qfiles.h src/tools/asm/opstrings.h
